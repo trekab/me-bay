75d351655a39f3ccbf5e033eaef5d33a8f6beb24
Treasure Kabareebe <treasurecalm@gmail.com>
2020-09-27 06:29:22 +0300

    Add a button that causes a product to be posted to a cart, resulting in a new line item

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ApplicationRecord
-  has_many :items
+  has_many :line_items
   before_destroy :ensure_not_referenced_by_any_line_item
   validates :title, :description, :image_url, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0.01 }
